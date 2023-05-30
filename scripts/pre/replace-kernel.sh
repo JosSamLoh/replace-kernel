e70752c9ebb9181f718532d7ecd44ba45fc2891d
JosSamLoh <72921778+JosSamLoh@users.noreply.github.com>
2023-05-30 01:37:38 +0100

    Fixes
    Move back to base silverblue image

--- scripts/pre/replace-kernel.sh
+++ scripts/pre/replace-kernel.sh
@@ -6,7 +6,7 @@ set -oue pipefail
 # Set UNWANTED_PACKAGES=$(Query all installed packages listed as package names
 # only | grab entire lines containing 'kernel-tools' and
 # 'virtualbox-guest-additions')
-UNWANTED_PACKAGES="$(rpm -qa --qf "%{NAME}\n" | grep -P '^(?=kernel-tools|virtualbox-guest-additions)')"
+UNWANTED_PACKAGES="$(rpm -qa --qf "%{NAME}\n" | grep -P '^(?=kernel-tools|virtualbox|kmod)')"
 printf "### Packages to be removed ###\n$UNWANTED_PACKAGES\n\n"
 
 # Set INSTALLED_KERNEL_PACKAGES=$(Query all installed packages listed as
@@ -20,10 +20,9 @@ printf "### Packages to be replaced ###\n$INSTALLED_KERNEL_PACKAGES\n\n"
 FEDORA_VERSION="$(cat /usr/lib/os-release | grep -Po '(?<=VERSION_ID=)\d+')"
 printf "### Fedora version ###\n$FEDORA_VERSION\n\n"
 
-# Download all files recursively from  repo and output to /tmp/fsync-kernel-images
+# Download all files recursively from repo and output to /tmp/fsync-kernel-images
 wget -rc -np -nH -nd --random-wait -P "/tmp/fsync-kernel-images/" \
 	https://download.copr.fedorainfracloud.org/results/sentry/kernel-fsync/fedora-$FEDORA_VERSION-x86_64/
-
 printf "### kernel-fsync packages installed into ###\n/tmp/fsync-kernel-images/\n\n"
 
 # Move into the directory containing the sentry/kernel-fsync files
