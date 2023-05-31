5a15876852a4fe56eba8546f61fbe18142a8421b
JosSamLoh <72921778+JosSamLoh@users.noreply.github.com>
2023-05-31 01:58:49 +0100

    Temp remove script and fixes

--- scripts/pre/1-install-nvidia-repos.sh
+++ scripts/pre/1-install-nvidia-repos.sh
@@ -8,6 +8,3 @@ wget -P /tmp/rpms \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${RELEASE}.noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${RELEASE}.noarch.rpm
 printf "### RPMFusion repos installed ###\n"
-
-#rpm-ostree install akmod-nvidia xorg-x11-drv-nvidia-cuda #optional if using nvidia-smi or cuda
-#rpm-ostree kargs --append=rd.driver.blacklist=nouveau --append=modprobe.blacklist=nouveau --append=nvidia-drm.modeset=1
