d7fd3aa9a940da0d654f41cbedcfe7a330cfd665
JosSamLoh <72921778+JosSamLoh@users.noreply.github.com>
2023-05-31 05:39:23 +0100

    Update 0-replace-kernel.sh

--- scripts/pre/0-replace-kernel.sh
+++ scripts/pre/0-replace-kernel.sh
@@ -4,39 +4,44 @@
 set -oue pipefail
 
 # Set UNWANTED_PACKAGES=$(Query all installed packages listed as package names
-# only | grab lines containing 'kernel-tools', 'virtualbox', 'kmod', and
-# 'nvidia')
-UNWANTED_PACKAGES="$(rpm -qa --qf "%{NAME}\n" | grep -P '(?=kernel-tools|virtualbox|kmod|nvidia)')"
+# only | grab lines containing 'kernel-tools', 'virtualbox', 'kmod-nvidia')
+UNWANTED_PACKAGES="$(rpm -qa --qf "%{NAME}\n" | grep -P '(?=kernel-tools|virtualbox|kmod-nvidia)')"
 if [[ "$?" != 0 ]]; then
-    printf "### No problematic packages installed! ###\n"
+    printf "### No problematic packages installed! ###\n\n"
+    sleep 1
 else
     printf "### Packages are installed which may cause dependency issues ###\n$UNWANTED_PACKAGES\n"
-    printf "### Some of these packages can be 'rpm-ostree override remove'-d ###\n"
+    printf "### Some of these packages can be 'rpm-ostree override remove'-d ###\n\n"
+    sleep 2
 fi
 
 # Set INSTALLED_KERNEL_PACKAGES=$(Query all installed packages listed as
 # package names only | grab entire lines beginning with 'kernel*' except
 # 'kernel-tools')
 INSTALLED_KERNEL_PACKAGES="$(rpm -qa --qf "%{NAME}\n" | grep -P '^kernel(?!-tools).*')"
-printf "### Packages to be replaced ###\n$INSTALLED_KERNEL_PACKAGES\n"
 
 # Automatically determine which Fedora version we"re building.
 # Taken from build.sh - shortened
 FEDORA_VERSION="$(cat /usr/lib/os-release | grep -Po '(?<=VERSION_ID=)\d+')"
-printf "### Fedora version ###\n$FEDORA_VERSION\n"
+
+printf "### Packages to be replaced ###\n$INSTALLED_KERNEL_PACKAGES\n\n"
+printf "### Fedora version ###\n$FEDORA_VERSION\n\n"
+sleep 2
 
 # Download all files recursively from repo and output to
 # /tmp/fsync-kernel-images - So far I've not found a way to specify specific
 # files
-wget -rc -np -nH -nd --random-wait -P "/tmp/fsync-kernel-images/" \
-    https://download.copr.fedorainfracloud.org/results/sentry/kernel-fsync/fedora-$FEDORA_VERSION-x86_64/
-printf "### kernel-fsync packages installed into ###\n/tmp/fsync-kernel-images/\n"
+wget -rc -np -nH -nd --random-wait -P "/tmp/kernel-fsync/" \
+    "https://download.copr.fedorainfracloud.org/results/sentry/kernel-fsync/fedora-$FEDORA_VERSION-64_x86/"
+printf "### kernel-fsync rpms installed into ###\n/tmp/kernel-fsync/\n\n"
+sleep 1
 
 # Move into the directory containing the sentry/kernel-fsync files
-cd /tmp/fsync-kernel-images/
+cd /tmp/kernel-fsync/
 
 # Use rpm-ostree's cliwrap to allow dracut to run on the container and generate
 # an initramfs.
+### COMMENT OUT BELOW LINE IF USING LOCALLY ###
 rpm-ostree cliwrap install-to-root / && \
 # Replace all installed kernel packages with the respective fsync packages
 rpm-ostree override replace \
@@ -50,4 +55,4 @@ cd ..
 
 # Cleanup
 rm -r /tmp/fsync-kernel-images/
-printf "### Kernel replaced, temp files cleaned up ###\n"
+printf "### Kernel replaced, temp files cleaned up ###\n\n"
