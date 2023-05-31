765dc0272e30d19c0405bb78c6e8b6dc9ce59706
JosSamLoh <72921778+JosSamLoh@users.noreply.github.com>
2023-05-31 15:11:37 +0100

    Install rpmfusion script

---
+++ scripts/pre/1-install-rpmfusion-repos.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+set -oue pipefail
+
+# Automatically determine which Fedora version we"re building.
+# Taken from build.sh - shortened
+FEDORA_VERSION="$(cat /usr/lib/os-release | grep -Po '(?<=VERSION_ID=)\d+')"
+printf "### Fedora version ###\n$FEDORA_VERSION\n"
+
+# Get RPMFusion repo rpms
+wget -P /tmp/rpmfusion-repos/ \
+    https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$FEDORA_VERSION.noarch.rpm \
+    https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$FEDORA_VERSION.noarch.rpm
+
+rpm-ostree install \
+    /tmp/rpmfusion-repos/rpmfusion-free-release-$FEDORA_VERSION.noarch.rpm \
+    /tmp/rpmfusion-repos/rpmfusion-nonfree-release-$FEDORA_VERSION.noarch.rpm
